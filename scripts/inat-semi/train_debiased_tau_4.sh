cae41f46a47433338baa37e4d1a187c804c58e91
linzhiqiu <zl279@cornell.edu>
2022-10-09 13:24:14 -0400

    [wip] tail uniform

--- scripts/inat-semi/train_debiased_tau_4.sh
+++ scripts/inat-semi/train_debiased_tau_4.sh
@@ -14,12 +14,12 @@ python main_semi_supervised.py \
   --dist-url 'tcp://localhost:11001' --multiprocessing-distributed --world-size 1 --rank 0 \
   --self-pretrained pretrained/res50_moco_eman_800ep.pth.tar \
   --amp-opt-level O1 \
-  --threshold 0.99 \
-  --tau 0.4 \
+  --threshold 0.95 \
+  --tau 0.9 \
   --train-type RandAugment \
   --weak-type DefaultTrain \
   --strong-type RandAugment \
   --multiviews \
   --qhat_m 0.99 \
-  --output checkpoints/semi_supervised/inat_debiased_3_tau_1/ \
+  --output checkpoints/semi_supervised/inat_debiased_tau_4/ \
   --cls 810 \
